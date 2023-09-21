cf683599819539ff1728592a226cbc4e9b086da8
ajdvz0423 <ajdvz0423@gmail.com>
2023-09-21 12:25:52 +0800

    try fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 const getParkrunUser = async () => {
-  const res = await fetch("/api/getParkrun", {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    "parkrun-nextjs-8cjszvj6r-ajdvz0423.vercel.app/api/getParkrun",
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
